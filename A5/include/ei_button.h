eb0ba5e46f91fc9c1435c0abe1b6e30beb7d4473
delaitea <antoine.delaite@grenoble-inp.fr>
2014-06-04 13:46:37 +0200

    un push

--- A5/include/ei_button.h
+++ A5/include/ei_button.h
@@ -10,7 +10,9 @@ typedef enum {
 	basse = -1
 } part_rect_t;
 
-ei_linked_point_t* ei_button_arc(ei_point_t centre,int rayon,int angle_debut,int angle_fin);
+ei_linked_point_t* ei_button_arc(ei_point_t centre,int rayon,int angle_debut,int angle_fin,ei_linked_point_t* suivant);
+
+ei_linked_point_t* trait(ei_point_t queue, ei_point_t tete, ei_linked_point_t* suivant);
 
 ei_linked_point_t* ei_button_rounded_frame(ei_rect_t rectangle, int rayon, part_rect_t partie);
 
