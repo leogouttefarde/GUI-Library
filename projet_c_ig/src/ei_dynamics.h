718da55d5060ad0e5bf2739e0910691056c840a8
Eric BUREL <eric.burel@ensimag.grenoble-inp.fr>
2014-06-13 10:59:03 +0200

    avec les fichiers'

---
+++ projet_c_ig/src/ei_dynamics.h
@@ -0,0 +1,14 @@
+#ifndef EI_DYNAMICS_H
+#define EI_DYNAMICS_H
+#include "ei_types.h"
+#include "ei_widget.h"
+#include "ei_widgettypes.h"
+#include "ei_params.h"
+#include "ei_common.h"
+#include <stdlib.h>
+#include <assert.h> 
+void resize(ei_widget_t *widget, ei_size_t add_size);
+void move(ei_widget_t *widget, ei_size_t dist);
+
+
+#endif
